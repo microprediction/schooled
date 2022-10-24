7d89122580a0596dce4933ea4288b7a9630c2ea3
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2022-10-24 12:59:24 -0400

    Update prerun.sh

--- prerun.sh
+++ prerun.sh
@@ -3,5 +3,5 @@ python3 -m pip install --update pip
 python3 -m pip install --upgrade sktime
 python3 -m pip install --upgrade pmdarima
 python3 -m pip install --upgrade statsforecast
-python3 -m pip install --upgrade timemachines
+python3 -m pip install --upgrade git+https://github.com/microprediction/timemachines.git
 pip install -e . 
