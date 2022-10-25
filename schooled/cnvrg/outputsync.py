f2774ccd7a2582df58d27b352893fca41d378b3f
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2022-10-25 13:21:23 -0400

    Update outputsync.py

--- schooled/cnvrg/outputsync.py
+++ schooled/cnvrg/outputsync.py
@@ -3,6 +3,7 @@ from schooled.wherami import RUNNING_LOCALLY
 if not RUNNING_LOCALLY:
 
     def sync_previous():
+        print('sync_previous')
         from cnvrgv2 import Cnvrg
         cnvrg = Cnvrg()
         proj = cnvrg.projects.get("project_name")
@@ -16,6 +17,8 @@ if not RUNNING_LOCALLY:
         for i in range(len(fv.info()["fv_status"]["task_statuses"][0]["experiments"])):
             e = proj.experiments.get(fv.info()["fv_status"]["task_statuses"][0]["experiments"][i]["id"])
             e.pull_artifacts(wait_until_success=True, poll_interval=5)
+        
+        print('sync_previous done')
 
 else:
     def sync_previous():
