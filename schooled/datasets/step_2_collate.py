c53298cdf7f43b76d17bfa1d5225c6b5d30ef43c
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2022-10-26 00:15:34 -0400

    Update step_2_collate.py

--- schooled/datasets/step_2_collate.py
+++ schooled/datasets/step_2_collate.py
@@ -1,10 +1,13 @@
 import pandas as pd
-from schooled.whereami import INPUT_CSVS, OUTPUT_DIR
-
+from schooled.whereami import OUTPUT_DIR
+import time
 
 def collate_arima_csv():
     from schooled.cnvrg.outputsync import sync_previous
     sync_previous(flow_name='sarima')
+    time.sleep(1)
+    INPUT_CSVS = glob(OUTPUT_DIR+'/*/output/*.csv', recursive=False)
+    print({'input_csv_files':INPUT_CSVS[:2],'num_files':len(INPUT_CSVS)})
     dfs = [pd.read_csv(fn, header=None) for fn in INPUT_CSVS ]
     df = pd.concat(dfs, ignore_index=True)
     df.to_csv(OUTPUT_DIR+'/collated.csv', header=False, index=False)
