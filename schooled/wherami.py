ddd8d9c26ab10a9917c916db043f8a13736fd83b
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2022-10-25 22:33:43 -0400

    Update wherami.py

--- schooled/wherami.py
+++ schooled/wherami.py
@@ -15,7 +15,7 @@ if RUNNING_LOCALLY:
 else:
     OUTPUT_DIR = '/cnvrg/output'
     try:
-        input_dirs = glob('/input/*/', recursive=False)
+        input_dirs = glob('/output/*/', recursive=False)
         INPUT_DIR = input_dirs[0]+'output'
         INPUT_CSV = glob(INPUT_DIR+'/*.csv', recursive=False)
         print({'INPUT_DIR':INPUT_DIR,'input_csv_files':INPUT_CSV[:2],'num_files':len(INPUT_CSV)})
