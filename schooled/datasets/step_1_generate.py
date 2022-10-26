cfed09138df65f02b8bf6370145e663d0adc19ba
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2022-10-26 00:33:02 -0400

    Update step_1_generate.py

--- schooled/datasets/step_1_generate.py
+++ schooled/datasets/step_1_generate.py
@@ -5,10 +5,10 @@ import pandas as pd
 from schooled.datasets.ornstein import simulate_arima_like_path
 from schooled.datasets.filenameconventions import generated_csv
 
-NUM_ROWS = 1
+NUM_ROWS = 100
 SEQ_LEN = 100
 
-DEBUG_FLOW = True
+DEBUG_FLOW = False
 
 
 def skater_single_prediction(ys, f):
